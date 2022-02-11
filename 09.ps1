9c6238638d68e0e583a5c7dbfa7dc04a90eb5a3d
Aniza07 <95593012+Aniza07@users.noreply.github.com>
2022-02-11 19:29:57 +0100

    Ej 9

--- 09.ps1
+++ 09.ps1
@@ -1,1 +1,10 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
+$numero1=Read-Number "Digues el primer numero"
+$numero2=Read-Number "Digues el segon numero"
+
+
+if ($numero1=$numero2){
+     Write-output 'OK'
+}
+else { Write-Output 'KO'
+}
